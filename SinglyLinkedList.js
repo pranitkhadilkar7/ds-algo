ba1bb1857ca9dc1574f12c03346d118e9255f1c4
Pranit Khadilkar <pranit@emtech.com>
2024-02-03 11:31:04 +0530

    added node class

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -1,5 +1,14 @@
+class Node {
+    constructor (val) {
+        this.value = val
+        this.next = null
+    }
+}
+
 class SinglyLinkedList {
     constructor () {
-        
+        this.head = null
+        this.tail = null
+        this.length = 0
     }
 }
