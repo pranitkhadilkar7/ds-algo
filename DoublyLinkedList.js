93ff6df75b6f73e2dcc75bc53b6d15061084795a
Pranit Khadilkar <pranit@emtech.com>
2024-02-08 18:57:20 +0530

    created doubly linked list class with push method

---
+++ DoublyLinkedList.js
@@ -0,0 +1,31 @@
+class Node {
+    constructor (val) {
+        this.value = val
+        this.next = null
+        this.prev = null
+    }
+}
+
+class DoublyLinkedList {
+    constructor () {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    push (val) {
+        const node = new Node(val)
+        if (!this.head) {
+            this.head = node
+            this.tail = node
+        } else {
+            this.tail.next = node
+            node.prev = this.tail
+            this.tail = node
+        }
+        this.length++
+        return this
+    }
+}
+
+const list = new DoublyLinkedList()
