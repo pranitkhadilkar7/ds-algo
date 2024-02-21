7cb36ae6363804ead48f1d67267a32074513c4d1
Pranit Khadilkar <pranit@emtech.com>
2024-02-21 10:11:38 +0530

    added extact max method in heap

--- MaxHeap.js
+++ MaxHeap.js
@@ -1,6 +1,6 @@
 class MaxBinaryHeap {
     constructor () {
-        this.values = [41, 39, 33, 18, 27, 12]
+        this.values = [55, 39, 41, 18, 27, 12, 33]
     }
 
     insert (val) {
@@ -16,4 +16,47 @@ class MaxBinaryHeap {
             currentIndex = parentIndex
         }
     }
+
+    extractMax () {
+        if (!this.values.length) return undefined
+
+        const max = this.values[0]
+        const end = this.values.pop()
+        if (!this.values.length) return undefined
+
+        this.values[0] = end
+        let length = this.values.length
+        let currentIndex = 0
+        const element = this.values[currentIndex]
+        while (true) {
+            const leftChildIndex = (2 * currentIndex) + 1
+            const rightChildIndex = (2 * currentIndex) + 2
+            let leftElement, rightElement
+            let swapIndex = null
+
+            if (leftChildIndex < length) {
+                leftElement = this.values[leftChildIndex]
+                if (leftElement > element) {
+                    swapIndex = leftChildIndex 
+                }
+            }
+
+            if (rightChildIndex < length) {
+                rightElement = this.values[rightChildIndex]
+                if ((!swapIndex && rightElement > element) ||
+                    (swapIndex && rightElement > leftElement)) {
+                        swapIndex = rightChildIndex
+                    }
+            }
+
+            if (!swapIndex) break
+
+            this.values[currentIndex] = this.values[swapIndex]
+            this.values[swapIndex] = element
+            currentIndex = swapIndex
+        }
+        return max
+    }
 }
+
+const heap = new MaxBinaryHeap()
