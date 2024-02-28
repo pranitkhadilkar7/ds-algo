9788257052b7f0e19d269f9cc88f62b715f75f5f
Pranit Khadilkar <pranit@emtech.com>
2024-02-28 09:38:33 +0530

    added remove edge method for the graph

--- Graph.js
+++ Graph.js
@@ -13,4 +13,16 @@ class Graph {
             this.adjacencyList[vertex2].push(vertex1)
         }
     }
+
+    removeEdge (vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(vertex => vertex !== vertex2)
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(vertex => vertex !== vertex1)
+    }
 }
+
+const graph = new Graph()
+graph.addVertex('Tokyo')
+graph.addVertex('NYC')
+graph.addVertex('Dallas')
+graph.addEdge('Tokyo', 'Dallas')
+graph.addEdge('Tokyo', 'NYC')
