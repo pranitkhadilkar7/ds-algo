5381c53502338891c816f1c89f63036b860ec506
Pranit Khadilkar <pranit@emtech.com>
2024-02-28 09:23:33 +0530

    added addEdge for the graph

--- Graph.js
+++ Graph.js
@@ -6,4 +6,11 @@ class Graph {
     addVertex (value) {
         if (!this.adjacencyList[value]) this.adjacencyList[value] = []
     }
+
+    addEdge (vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1].push(vertex2)
+            this.adjacencyList[vertex2].push(vertex1)
+        }
+    }
 }
