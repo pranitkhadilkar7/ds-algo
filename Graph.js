cd267ec4d44853f4748337f78fe8695afc9d5737
Pranit Khadilkar <pranit@emtech.com>
2024-02-28 11:09:53 +0530

    added depth first search using recursion for graph

--- Graph.js
+++ Graph.js
@@ -26,11 +26,38 @@ class Graph {
         }
         delete this.adjacencyList[vertex]
     }
+
+    DFS (start) {
+        const result = []
+        const visited = {}
+        const adjacencyList = this.adjacencyList
+
+        function traverse (vertex) {
+            result.push(vertex)
+            visited[vertex] = true
+            for (let child of adjacencyList[vertex]) {
+                if (!visited[child]) {
+                    traverse(child)
+                }
+            }
+        }
+        traverse(start)
+        return result
+    }
 }
 
 const graph = new Graph()
-graph.addVertex('Tokyo')
-graph.addVertex('NYC')
-graph.addVertex('Dallas')
-graph.addEdge('Tokyo', 'Dallas')
-graph.addEdge('Tokyo', 'NYC')
+graph.addVertex('A')
+graph.addVertex('B')
+graph.addVertex('C')
+graph.addVertex('D')
+graph.addVertex('E')
+graph.addVertex('F')
+
+graph.addEdge('A', 'B')
+graph.addEdge('A', 'C')
+graph.addEdge('B', 'D')
+graph.addEdge('C', 'E')
+graph.addEdge('D', 'E')
+graph.addEdge('D', 'F')
+graph.addEdge('E', 'F')
